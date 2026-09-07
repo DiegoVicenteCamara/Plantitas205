ea7d98c4faa3457e3d4479a9d1067f5a932da875
DiegoVicenteCamaraG4 <diego.vic.cam@gmail.com>
2026-09-07 18:36:34 +0200

    Remove unused PlantSearchCriteria DTO and configure H2 database properti

--- backend/src/test/java/com/plantitas/controller/PlantSearchIntegrationTest.java
+++ backend/src/test/java/com/plantitas/controller/PlantSearchIntegrationTest.java
@@ -12,7 +12,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.datasource.url=jdbc:h2:mem:plantitasdb-test;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE")
 @AutoConfigureMockMvc
 class PlantSearchIntegrationTest {
 
